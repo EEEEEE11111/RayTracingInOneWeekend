38b546368ad2fc252a2d8c74e883d8ea964dce91
lavender <1690486028@qq.com>
2025-04-26 23:10:15 -0400

    lambertian reflection

--- camera.h
+++ camera.h
@@ -87,10 +87,10 @@ private:
         }
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec))
+        if (world.hit(r, interval(0.001, infinity), rec))
         {
-            vec3 direction = random_on_semisphere(rec.normal);
-            return 0.5 * (ray_color(ray(rec.p, direction), depth - 1, world));
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.7 * (ray_color(ray(rec.p, direction), depth - 1, world));
         }
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5 * (unit_direction.y() + 1.0);
